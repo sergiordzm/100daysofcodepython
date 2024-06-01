c82f9c7c52e56b3e142270b49d97d365d6c1afe0
Sergio <smrma1308@gmail.com>
2024-05-31 23:04:56 -0600

    working day two

--- day-2.py
+++ day-2.py
@@ -2,25 +2,38 @@
 
 # String
 
-a = 'Hola, mundo!'
+# a = 'Hola, mundo!'
 
-print(a[0])#Para imprimir un caracter específico de una cadena de caracteres (subscript)
-print(a[10])
+# print(a[0])#Para imprimir un caracter específico de una cadena de caracteres (subscript)
+# print(a[10])
 
-# Integer
+# # Integer
 
-b = 12
-c = 18
+# b = 12
+# c = 18
 
-print(b + c)
+# print(b + c)
 
-# Float
+# # Float
 
-d = 12.8
-e = 18.5
+# # d = 12.8
+# # e = 18.5
 
-print(d + e)
+# # print(d + e)
 
-# Boolean
-f = True
-g = False
+# # Boolean
+# f = True
+# g = False
+
+# num_char = len(input('Cuál es tu nombre?'))
+
+# new_num_char = str(num_char)
+
+# print('Tu nombre tiene: ' + new_num_char + ' letras')
+
+# print (type(num_char))
+
+# a = float(123)
+
+
+# print(type(a))

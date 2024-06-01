a8cba947d9e3cd8b2cdb2952889ba864259840d7
Sergio <smrma1308@gmail.com>
2024-05-31 22:41:58 -0600

    day two

---
+++ day-2.py
@@ -0,0 +1,26 @@
+# Data Types
+
+# String
+
+a = 'Hola, mundo!'
+
+print(a[0])#Para imprimir un caracter específico de una cadena de caracteres (subscript)
+print(a[10])
+
+# Integer
+
+b = 12
+c = 18
+
+print(b + c)
+
+# Float
+
+d = 12.8
+e = 18.5
+
+print(d + e)
+
+# Boolean
+f = True
+g = False

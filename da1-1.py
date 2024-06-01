cde107be4e050740c81cefb377502cc7d3e772ab
Sergio <smrma1308@gmail.com>
2024-05-31 22:41:02 -0600

    day one

---
+++ da1-1.py
@@ -0,0 +1,20 @@
+# print('Hello world! \nHello world! \nHello world!')
+# print("Hola" + " "+ "Sergio")
+
+# Funcion input
+# name = input('Cuál es tu nombre?')
+# print('Hola ' + name)
+# print('Hola ' + input('Cuál es tu nombre?'))
+
+# Para contar el numero de letras de una palabra se utiliza el metodo len()
+# print('El numero de letras de tu nombre es: ' + str(len(input('Cual es tu nombre? '))))
+
+# Variables
+
+# name = input('Cuál es tu nombre?')
+# print(name)
+print('Bienvenido al programa NombraTuBanda')
+name_city = input('Cuál es el nombre de tu ciudad?\n')
+name_pet = input('Cuál es el nombre de tu mascota?\n')
+
+print('El nombre de tu banda es: ' + name_city+' '+name_pet)
